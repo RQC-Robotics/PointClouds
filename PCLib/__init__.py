c8d9cf7e3734a78e6079b928838241482e70d923
RQC-Robotics <95026214+RQC-Robotics@users.noreply.github.com>
2022-02-25 17:57:16 +0300

    Update __init__.py

--- PCLib/__init__.py
+++ PCLib/__init__.py
@@ -1,2 +1,2 @@
-from .nets import *
-from .sim import * # generate_pressure_map, fiber_sim, sim_on_gpu, prepare_dataset_for_train
+from .models import *
+from .tools import * # generate_pressure_map, fiber_sim, sim_on_gpu, prepare_dataset_for_train
