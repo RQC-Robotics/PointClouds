39ffb8d2d3e317607f6f35bf56e09af3a708212e
RQC-Robotics <95026214+RQC-Robotics@users.noreply.github.com>
2022-02-25 18:10:59 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -7,5 +7,5 @@ setup(
     author='Artem',
     author_email='artl2sch@gmail.com',
     packages=find_packages(),
-    long_description=open(join(dirname(__file__), 'README.txt')).read(),
+    long_description=open(join(dirname(__file__), 'README.md')).read(),
 )
